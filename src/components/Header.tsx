c6212a4faeb5b2d5adde9a2d248b7f4516b46060
samuel5721 <samuel20070731@gmail.com>
2024-03-05 20:35:03 +0900

    bug fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,11 @@ const Header = () => {
   return (
     <>
       <Row style={{ padding: '1rem 6rem' }} gap={'4rem'} justify="space-between">
-        <Title color="--gray-900" $bold>
-          #IMPLUDE
-        </Title>
+      <Link to="/">
+          <Title color="--gray-900" $bold>
+            #IMPLUDE
+          </Title>
+        </Link>
         <Row gap={'4rem'} align="center">
           <Link to="/history"><Header1 color="--gray-700">역사</Header1></Link>
           <Link to="/vision"><Header1 color="--gray-700">핵심가치</Header1></Link>
--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,7 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled, { css } from 'styled-components'
+import { Blank, Col, Description, Header1, Row, Title } from '../../components/atomic'
+
 export default function MainPage() {
   return (
-    <div>
-      <h1>메인페이지</h1>
-    </div>
+    <Wrapper>
+      <Row gap='100%'>
+        <div>예아</div>
+        <Col>
+          <div>히</div>
+          <div>호</div>
+        </Col>
+      </Row>
+    </Wrapper>
   )
+
+  
 }
