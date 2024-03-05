98b61f80e64c03a63cbdc3c140f8536647a721c1
samuel5721 <samuel20070731@gmail.com>
2024-03-05 22:55:56 +0900

    feat: recruit layout

--- src/pages/Recruit/RecruitPage.jsx
+++ src/pages/Recruit/RecruitPage.jsx
@@ -5,6 +5,9 @@ import styled, { css } from 'styled-components'
 import { Blank, Col, Wrap, Description, Header1, Row, Title } from '../../components/atomic'
 
 export default function RecruitPage() {
+  const [part, setPart] = useState(0);
+
+
   return (
     <Col>
       <BannerImage src='' />
@@ -19,11 +22,11 @@ export default function RecruitPage() {
             <Header1 color={'--gray-black'}>임플루드와 함께하기로 결정하셨다면, 임플루드 합류 여정을 소개해드릴게요!</Header1>
             <Blank height='1rem'/>
             <Row style={{position:'relative'}}>
-              <TitleCircle style={{background: '#99c8ff'}}></TitleCircle>
-              <TitleCircle style={{background: '#005ecc', left:'8rem'}}></TitleCircle>
-              <TitleCircle style={{background: '#002f66', left:'16rem'}}></TitleCircle>
-              <TitleCircle style={{background: '#3391ff', left:'24rem'}}></TitleCircle>
-              <TitleCircle style={{background: 'black', left:'32rem'}}></TitleCircle>
+              <TitleCircle style={{background: '#99c8ff'}}><CircleText color='black'>지원서<br />접수</CircleText></TitleCircle>
+              <TitleCircle style={{background: '#3391ff', left:'8rem'}}><CircleText color='black'>지원서<br />검토</CircleText></TitleCircle>
+              <TitleCircle style={{background: '#005ecc', left:'16rem'}}><CircleText color='--gray-white'>지원서<br />결과 발표</CircleText></TitleCircle>
+              <TitleCircle style={{background: '#002f66', left:'24rem'}}><CircleText color='--gray-white'>면접</CircleText></TitleCircle>
+              <TitleCircle style={{background: 'black', left:'32rem'}}><CircleText color='--gray-white'>최종<br />발표</CircleText></TitleCircle>
             </Row>
             <Blank height='11rem'/>
             <Description color={'--gray-400'}>*상황에 따라 유동적으로 변경될 수 있어요.</Description>
@@ -47,10 +50,11 @@ export default function RecruitPage() {
               <li>지원서에는 자신의 성장 가능성을 구체적으로 알아볼 수 있는 내용이 들어가면 좋아요!</li>
             </Description>
             <Description color={'--gray-black'}>지원서는 소중한 시간을 할애해 지원해주신 만큼 꼼꼼하게 살펴보고 있어요!</Description>
-            <div style={{textAlign:'center'}}>
-              <button>지원서 작성 바로가기</button>
-            </div>
+            <div style={{display:'flex', justifyContent:'center'}}>
+            <ApplyButton><PartButtonText style={{color:'white'}}>지원서 작성 바로가기</PartButtonText></ApplyButton>
+          </div>
           </>
+          
           <Blank height='4.5rem'/>
           <>
             <Title color={'--gray-black'}>3. 면접 보기</Title>
@@ -69,13 +73,39 @@ export default function RecruitPage() {
             <Title color={'--gray-black'}>모집 분야 자세히 보기</Title>
             <Blank height='5rem'/>
             <Row gap='1rem' justify='center'>
-              <button>개발자</button>
-              <button>디자이너</button>
-              <button>기획자</button>
+              <PartButton
+                style={{backgroundColor:(part==0) ? '#005ecc' : 'var(--gray-200)'}}
+                onClick={() => {setPart(0)}}
+              >
+                <PartButtonText style={{color:(part==0) ? 'white' : 'var(--gray-600)'}}>개발자</PartButtonText>
+              </PartButton>
+              <PartButton
+                style={{backgroundColor:(part==1) ? '#005ecc' : 'var(--gray-200)'}}
+                onClick={() => {setPart(1)}}
+              >
+                <PartButtonText style={{color:(part==1) ? 'white' : 'var(--gray-600)'}}>디자이너</PartButtonText>
+              </PartButton>
+              <PartButton
+                style={{backgroundColor:(part==2) ? '#005ecc' : 'var(--gray-200)'}}
+                onClick={() => {setPart(2)}}
+              >
+                <PartButtonText style={{color:(part==2) ? 'white' : 'var(--gray-600)'}}>기획자</PartButtonText>
+              </PartButton>
             </Row>
+            <Blank height='5rem'/>
+            {part == 0 && <>
+              se
+            </>}
+            {part == 1 && <>
+              sa
+            </>}
+            {part == 2 && <>
+              <PartBox>
+                asoifsdaiofsjfsdakj
+              </PartBox>
+            </>}
+            <Blank height='7.5rem'/>
           </div>
-          <>
-          </>
         </Col>
       </div>
   </Col>
@@ -92,9 +122,61 @@ const BannerImage = styled.img`
 `
 
 const TitleCircle = styled.div`
+  display: flex;
   width : 10rem;
   height : 10rem;
   border-radius : 10rem;
   position : absolute;
+  text-align : center;
+  justify-content : center;
+  align-items: center;
+`
 
+const CircleText = styled(Title)`
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`
+
+const ApplyButton = styled.button`
+  display: flex;
+  padding: 0.25rem 3.625rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  flex-shrink: 0;
+  border: none;
+  border-radius: 3.1875rem;
+  background: var(--Gray-Brand-600, #005ECC);
+`
+
+const PartButton = styled.button`
+  display: flex;
+  padding: 0.25rem 3.625rem;
+  align-items: center;
+  gap: 19.9375rem;
+  border-radius: 3.5625rem;
+  border: none;
+`
+
+const PartButtonText = styled.p`
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`
+
+const PartBox = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 2.5rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 5rem;
+  border-radius: 1rem;
+  background: var(--Gray-0, #FBFBFB);
 `
