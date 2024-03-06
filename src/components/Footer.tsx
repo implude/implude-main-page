226583e99554c1bcab9f756e1bb5973c881fc6ba
in_queue <77333101+bkkim7421@users.noreply.github.com>
2024-03-06 22:20:07 +0800

    OwO (#7)
    * Feat: to rem
    
    * Feat: 미디어 쿼리 모바일
    
    * Feat: Circle Refactoring
    
    * ..

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <Container>
         <Wrap justify="space-between" gap="2rem">
           <Wrap gap={'1rem'} align="center">
-            <Implude />
+            <Implude height="6.25rem" width="6.25rem" />
             <Col align="start">
               <Title color="--gray-white" $bold>
                 #IMPLUDE
@@ -24,10 +24,10 @@ const Footer = () => {
           </Wrap>
           <Row gap={'2rem'}>
             <Link to={'https://www.youtube.com/@implude_official/featured'} style={{ cursor: 'pointer' }}>
-              <Youtube />
+              <Youtube height="6.25rem" width="6.25rem" />
             </Link>
             <Link to={'https://www.instagram.com/implude_official/'} style={{ cursor: 'pointer' }}>
-              <Instagram />
+              <Instagram height="6.25rem" width="6.25rem" />
             </Link>
           </Row>
         </Wrap>
@@ -35,7 +35,7 @@ const Footer = () => {
           <Info>
             상호 : #IMPLUDE | 대표자명 : 김진욱, 박지윤
             <br />
-            이메일 : 몰라몰라@implude.com
+            이메일 : biz.implude@dimigo.hs.kr
             <br />
             주소 : 경기도 안산시 단원구 사세충열로 94
           </Info>
